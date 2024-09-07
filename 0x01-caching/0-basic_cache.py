a8b3976ba67c97ab92b9e669077ec54d217b2719
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-07 14:43:08 +0300

    cahcing update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-""" """
+"""Craete a basic cache"""
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    """
+    """implements setting up, adding and retriving a cache"""
+
     def __init__(self):
-        """call super from Parent
-        """
+        """call super from Parent"""
         super().__init__()
 
     def put(self, key, item):
-        """
-        """
+        """add something to the cache"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        """
+        """retrieve something from the cache"""
         if not key:
             return None
         if self.cache_data.get(key):
