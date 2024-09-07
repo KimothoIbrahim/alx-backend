960e83fd50ceb64d6a7a094963b6e29f2c8e77ed
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-09-07 15:30:54 +0300

    Implement a fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Implement a Fifo cache"""
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """make a fifo cache
+    """
+
+    def __init__(self):
+        """initilize FIFOCache"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ add something to cache """
+        if key or item:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            firstKey = next(iter(self.cache_data))
+            del self.cache_data[firstKey]
+            print("DISCARD:", firstKey)
+
+    def get(self, key):
+        """ retrieve item from cache """
+        if not key:
+            return None
+        if self.cache_data.get(key):
+            return self.cache_data.get(key)
+        else:
+            return None
