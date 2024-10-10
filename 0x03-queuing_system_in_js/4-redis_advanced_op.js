83bd4ea13cffb6052209100f90f0a2b3b5d41d3b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2024-10-10 18:36:12 +0300

    Hset in Node redis

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,17 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => console.log(err));
+client.on('connect', () => console.log("Redis client connected to the server"));
+
+
+client.hset("HolbertonSchools",                                                                                           'Portland', '50',
+  'Seattle', '80',
+  'New York', '20',
+  'Bogota', '20',
+  'Cali', '40',
+  'Paris', '2',
+  redis.print);
+
+client.hget("HolbertonSchools", 'Portland', (err, outcome) => {console.log('Portland', outcome)});
